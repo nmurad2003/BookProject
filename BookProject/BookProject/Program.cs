36701c2735789b99f1e924f8e1739515a348c25b
Murad Nurmammadov <muradin-bb104@code.edu.az>
2025-04-07 08:17:56 +0400

    Initial commit

---
+++ BookProject/BookProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BookProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
